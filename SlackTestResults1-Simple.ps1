231073ac2503c62376c276f8b5d2f62bcaa3b61d
DanStach <Stachd@gmail.com>
2018-07-23 21:20:33 -0500

    Example of a simple slack message sent via powershell

---
+++ SlackTestResults1-Simple.ps1
@@ -0,0 +1,15 @@
+﻿# Example of a simple slack message sent via powershell
+
+$Input_Testenvironment = "PRD"
+$Input_TestURL = "https://www.google.com"
+$Input_Category = "Smoke"
+
+$testsTotal = 100
+$testsPassed = 99
+$testsFailed = 1
+
+$SlackWebHookUri = "https://hooks.slack.com/services/<your webhook info>" 
+$Payload = @{ "text" = "$Input_Testenvironment - $Input_TestURL - UI tests finished for $Input_Category
+TOTAL=$testsTotal, PASSED=$testsPassed, FAILED=$testsFailed";}
+
+Invoke-RestMethod -Method Post -Uri $SlackWebHookUri -Body (ConvertTo-Json -InputObject $Payload)
